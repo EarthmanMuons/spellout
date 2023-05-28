d86ce08763ee1bded200fb40599b2b2dba8ac7c4
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-05-28 06:41:46 -0700

    Use constants intead of lazy initialization and organize files
    Since we're not accessing the HashMap frequently, and are just
    assembling a more comprehensive conversion map, it makes more sense to
    define them as constants.

--- crates/spellabet/src/lib.rs
+++ crates/spellabet/src/lib.rs
@@ -3,104 +3,30 @@
 
 use std::collections::HashMap;
 
-use once_cell::sync::Lazy;
+use code_words::{DEFAULT_DIGITS_AND_SYMBOLS, NATO_ALPHABET};
 
-pub static NATO_ALPHABET: Lazy<HashMap<char, &'static str>> = Lazy::new(|| {
-    let mut map = HashMap::new();
-    map.insert('a', "Alpha");
-    map.insert('b', "Bravo");
-    map.insert('c', "Charlie");
-    map.insert('d', "Delta");
-    map.insert('e', "Echo");
-    map.insert('f', "Foxtrot");
-    map.insert('g', "Golf");
-    map.insert('h', "Hotel");
-    map.insert('i', "India");
-    map.insert('j', "Juliet");
-    map.insert('k', "Kilo");
-    map.insert('l', "Lima");
-    map.insert('m', "Mike");
-    map.insert('n', "November");
-    map.insert('o', "Oscar");
-    map.insert('p', "Papa");
-    map.insert('q', "Quebec");
-    map.insert('r', "Romeo");
-    map.insert('s', "Sierra");
-    map.insert('t', "Tango");
-    map.insert('u', "Uniform");
-    map.insert('v', "Victor");
-    map.insert('w', "Whiskey");
-    map.insert('x', "X-ray");
-    map.insert('y', "Yankee");
-    map.insert('z', "Zulu");
-    map.insert('0', "Zero");
-    map.insert('1', "One");
-    map.insert('2', "Two");
-    map.insert('3', "Three");
-    map.insert('4', "Four");
-    map.insert('5', "Five");
-    map.insert('6', "Six");
-    map.insert('7', "Seven");
-    map.insert('8', "Eight");
-    map.insert('9', "Nine");
-    map.insert(' ', "Space");
-    map.insert('!', "Exclamation");
-    map.insert('"', "DoubleQuote");
-    map.insert('#', "Hash");
-    map.insert('$', "Dollars");
-    map.insert('%', "Percent");
-    map.insert('&', "Ampersand");
-    map.insert('(', "LeftParens");
-    map.insert(')', "RightParens");
-    map.insert('*', "Asterisk");
-    map.insert('+', "Plus");
-    map.insert(',', "Comma");
-    map.insert('-', "Dash");
-    map.insert('.', "Period");
-    map.insert('/', "ForeSlash");
-    map.insert(':', "Colon");
-    map.insert(';', "SemiColon");
-    map.insert('<', "LessThan");
-    map.insert('=', "Equals");
-    map.insert('>', "GreaterThan");
-    map.insert('?', "Question");
-    map.insert('@', "At");
-    map.insert('[', "LeftBracket");
-    map.insert('\'', "SingleQuote");
-    map.insert('\\', "BackSlash");
-    map.insert(']', "RightBracket");
-    map.insert('^', "Caret");
-    map.insert('_', "Underscore");
-    map.insert('`', "Backtick");
-    map.insert('{', "LeftBrace");
-    map.insert('|', "Pipe");
-    map.insert('}', "RightBrace");
-    map.insert('~', "Tilde");
-    map
-});
+mod code_words;
+
+pub struct PhoneticConverter {
+    conversion_map: HashMap<char, String>,
+}
 
 pub enum SpellingAlphabet {
     Nato,
 }
 
-pub struct PhoneticConverter {
-    alphabet: &'static HashMap<char, &'static str>,
-}
-
 impl PhoneticConverter {
     #[must_use]
-    pub fn new(alphabet_type: &SpellingAlphabet) -> Self {
-        let alphabet = match alphabet_type {
-            SpellingAlphabet::Nato => &*NATO_ALPHABET,
-        };
-        Self { alphabet }
+    pub fn new(alphabet: &SpellingAlphabet) -> Self {
+        let conversion_map = alphabet.initialize();
+        Self { conversion_map }
     }
 
     #[must_use]
     pub fn convert(&self, text: &str) -> String {
         let mut result = String::new();
         for c in text.chars() {
-            if let Some(word) = self.alphabet.get(&c.to_ascii_lowercase()) {
+            if let Some(word) = self.conversion_map.get(&c.to_ascii_lowercase()) {
                 if c.is_lowercase() {
                     result.push_str(&word.to_lowercase());
                 } else if c.is_uppercase() {
@@ -114,5 +40,19 @@ impl PhoneticConverter {
             result.push(' ');
         }
         result.trim_end().to_owned()
+    }
+}
+
+impl SpellingAlphabet {
+    #[must_use]
+    pub fn initialize(&self) -> HashMap<char, String> {
+        let mut map = DEFAULT_DIGITS_AND_SYMBOLS
+            .iter()
+            .map(|(k, v)| (*k, (*v).to_string()))
+            .collect::<HashMap<char, String>>();
+        match self {
+            Self::Nato => map.extend(NATO_ALPHABET.iter().map(|(k, v)| (*k, (*v).to_string()))),
+        };
+        map
     }
 }

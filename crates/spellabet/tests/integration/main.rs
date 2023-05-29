e8e60f596cfb626f40bc350e73ed9d24092c2de0
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-05-29 13:48:08 -0700

    Add with_overrides function to alter the conversion map

--- crates/spellabet/tests/integration/main.rs
+++ crates/spellabet/tests/integration/main.rs
@@ -1,16 +1,17 @@
 #![deny(clippy::all)]
 #![warn(clippy::nursery, clippy::pedantic)]
 
+use std::collections::HashMap;
+
 use spellabet::{PhoneticConverter, SpellingAlphabet};
 
 #[test]
 fn test_convert() {
     let alphabet = &SpellingAlphabet::Nato;
     let converter = PhoneticConverter::new(alphabet);
-    let input = "Example123";
 
     assert_eq!(
-        converter.convert(input),
+        converter.convert("Example123"),
         "ECHO x-ray alfa mike papa lima echo One Two Tree"
     );
 }
@@ -19,11 +20,37 @@ fn test_convert() {
 fn test_nonce_form() {
     let alphabet = &SpellingAlphabet::Nato;
     let converter = PhoneticConverter::new(alphabet).nonce_form(true);
-    let input = "Example123";
 
     assert_eq!(
-        converter.convert(input),
+        converter.convert("Example123"),
         "'E' as in ECHO, 'x' as in x-ray, 'a' as in alfa, 'm' as in mike, 'p' as in papa, 'l' as \
          in lima, 'e' as in echo, One, Two, Tree"
     );
+}
+
+#[test]
+fn test_with_overrides() {
+    let alphabet = &SpellingAlphabet::Nato;
+    let mut converter = PhoneticConverter::new(alphabet);
+
+    assert_eq!(converter.convert("a"), "alfa");
+    assert_eq!(converter.convert("A"), "ALFA");
+    assert_eq!(converter.convert("b"), "bravo");
+    assert_eq!(converter.convert("B"), "BRAVO");
+    assert_eq!(converter.convert("c"), "charlie");
+    assert_eq!(converter.convert("C"), "CHARLIE");
+
+    // Lowercase and uppercase keys will be normalized.
+    let mut overrides: HashMap<char, String> = HashMap::new();
+    overrides.insert('a', "Able".to_string());
+    overrides.insert('B', "Baker".to_string());
+
+    converter = converter.with_overrides(overrides);
+
+    assert_eq!(converter.convert("a"), "able");
+    assert_eq!(converter.convert("A"), "ABLE");
+    assert_eq!(converter.convert("b"), "baker");
+    assert_eq!(converter.convert("B"), "BAKER");
+    assert_eq!(converter.convert("c"), "charlie");
+    assert_eq!(converter.convert("C"), "CHARLIE");
 }

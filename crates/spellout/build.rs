681196b306b221035e9443fafeeeb881bd1dcd55
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-06-02 16:09:07 -0700

    Generate spellout package assets via build.rs
    To allow for easy parsing of the derived Clap command using the built-
    in CommandFactory trait, I moved the `Cli` struct and `Alphabet` enum
    into a dedicated file. This allows us to keep our build dependencies to
    a minimum.
    
    See:
    - https://docs.rs/clap_complete/latest/clap_complete/
    - https://docs.rs/clap_mangen/latest/clap_mangen/
    - https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts

---
+++ crates/spellout/build.rs
@@ -0,0 +1,60 @@
+use std::path::{Path, PathBuf};
+use std::{env, fs};
+
+use clap::CommandFactory;
+use clap_complete::{generate_to, Shell};
+use clap_mangen::Man;
+use cli::Cli;
+
+#[path = "src/cli.rs"]
+mod cli;
+
+type DynError = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), DynError> {
+    // Cargo sets the `OUT_DIR` environment variable to the folder in which all
+    // output and intermediate artifacts should be placed.
+    let out_dir = match env::var("OUT_DIR") {
+        Ok(val) => PathBuf::from(val),
+        Err(e) => {
+            eprintln!("OUT_DIR environment variable not defined");
+            return Err(e.into());
+        }
+    };
+
+    generate_completions(&out_dir)?;
+    generate_man_page(&out_dir)?;
+
+    Ok(())
+}
+
+fn generate_completions<P: AsRef<Path>>(out_dir: P) -> Result<(), DynError> {
+    let completions_dir = out_dir.as_ref().join("completions");
+    fs::create_dir_all(&completions_dir)?;
+
+    let mut cmd = Cli::command();
+    for shell in [
+        Shell::Bash,
+        Shell::Elvish,
+        Shell::Fish,
+        Shell::PowerShell,
+        Shell::Zsh,
+    ] {
+        generate_to(shell, &mut cmd, "spellout", &completions_dir)?;
+    }
+
+    Ok(())
+}
+
+fn generate_man_page<P: AsRef<Path>>(out_dir: P) -> Result<(), DynError> {
+    let man_dir = out_dir.as_ref().join("man");
+    fs::create_dir_all(&man_dir)?;
+
+    let cmd = Cli::command();
+    let man = Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+    fs::write(man_dir.join("spellout.1"), buffer)?;
+
+    Ok(())
+}
--- crates/spellout/src/main.rs
+++ crates/spellout/src/main.rs
@@ -3,61 +3,13 @@ use std::io::{self, BufRead};
 
 use anyhow::{Context, Result};
 use clap::error::{ContextKind, ContextValue, ErrorKind};
-use clap::{CommandFactory, Parser, ValueEnum};
+use clap::{CommandFactory, Parser};
 use is_terminal::IsTerminal;
 use spellabet::{PhoneticConverter, SpellingAlphabet};
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    /// Which spelling alphabet to use for the conversion
-    #[arg(short, long, env = "SPELLOUT_ALPHABET")]
-    #[arg(value_enum, default_value_t = Alphabet::Nato)]
-    alphabet: Alphabet,
+use crate::cli::{Alphabet, Cli};
 
-    /// Define overrides for spelling alphabet code words
-    ///
-    /// Provide a comma-separated list of character=word pairs like
-    /// "a=apple,b=banana" which will override the default values.
-    #[arg(short, long, env = "SPELLOUT_OVERRIDES")]
-    overrides: Option<String>,
-
-    /// Display the spelling alphabet and exit
-    ///
-    /// Shows only letters by default; add the `--verbose` flag to also show
-    /// digits and symbols.
-    #[arg(long)]
-    dump_alphabet: bool,
-
-    /// Expand output into nonce form like "'A' as in ALFA"
-    #[arg(short, long, env = "SPELLOUT_NONCE_FORM")]
-    #[arg(value_parser = clap::builder::FalseyValueParser::new())]
-    nonce_form: bool,
-
-    /// Use verbose output
-    ///
-    /// Include the input characters along with each line's output.
-    #[arg(short, long, env = "SPELLOUT_VERBOSE")]
-    #[arg(value_parser = clap::builder::FalseyValueParser::new())]
-    verbose: bool,
-
-    /// An input character string to convert into code words
-    ///
-    /// If no input strings are provided, the program reads lines from standard
-    /// input.
-    #[arg(value_name = "STRING")]
-    input: Vec<String>,
-}
-
-#[derive(Clone, Debug, ValueEnum)]
-enum Alphabet {
-    /// Los Angeles Police Department (LAPD)
-    Lapd,
-    /// North Atlantic Treaty Organization (NATO)
-    Nato,
-    /// United States Financial Industry
-    UsFinancial,
-}
+mod cli;
 
 fn main() -> Result<()> {
     let cli = Cli::parse();

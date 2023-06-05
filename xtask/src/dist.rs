c8da9594b47cef9faf3d161e622e0ad0bb58f3d7
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-06-05 15:57:39 -0700

    Add package version to the dist destination directory

--- xtask/src/dist.rs
+++ xtask/src/dist.rs
@@ -20,6 +20,7 @@ struct Package {
     #[allow(dead_code)]
     name: String,
     targets: Vec<Target>,
+    version: String,
 }
 
 #[derive(Debug, DeJson)]
@@ -36,8 +37,8 @@ pub fn dist(config: &Config) -> Result<()> {
     }
     let binaries = project_binaries(config)?;
 
-    for binary in &binaries {
-        let dest_dir = dist_dir().join(binary);
+    for (binary, version) in &binaries {
+        let dest_dir = dist_dir().join(format!("{binary}-{version}"));
         fs::create_dir_all(&dest_dir)?;
 
         build_binary(config, binary, &dest_dir)?;
@@ -139,7 +140,7 @@ fn generate_assets(config: &Config, binary: &str, dest_dir: &Path) -> Result<()>
     Ok(())
 }
 
-fn project_binaries(config: &Config) -> Result<Vec<String>> {
+fn project_binaries(config: &Config) -> Result<Vec<(String, String)>> {
     let sh = Shell::new()?;
     let mut binaries = Vec::new();
 
@@ -154,7 +155,7 @@ fn project_binaries(config: &Config) -> Result<Vec<String>> {
         for package in metadata.packages {
             for target in &package.targets {
                 if target.name != "xtask" && target.kind.contains(&"bin".to_string()) {
-                    binaries.push(target.name.clone());
+                    binaries.push((target.name.clone(), package.version.clone()));
                 }
             }
         }
